f92ff4ebf79238aeb22c7bf945a252275072b804
Kirill Vorobiev <kovvok070606@yandex.ru>
2026-05-09 21:15:42 +0000

    fix(model): add level validation in model Agent

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -4,4 +4,5 @@ class Agent < ApplicationRecord
   has_many :skills, through: :agent_skills
 
   validates :codename, presence: true, uniqueness: true
+  validates :level, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 10  }
 end
