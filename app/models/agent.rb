bae71bb64880887ac1b1322566eb74f3b26d387b
Kirill Vorobiev <kovvok070606@yandex.ru>
2026-05-09 21:11:06 +0000

    fix(model): add validation for codename field in model Agent

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -2,4 +2,6 @@ class Agent < ApplicationRecord
   has_many :missions
   has_many :agent_skills
   has_many :skills, through: :agent_skills
+
+  validates :codename, presence: true, uniqueness: true
 end
